a59b6f728bb8aedf33c0fcb973eeed955b55b41a
Danylo <cloudmenson9@gmail.com>
2025-06-08 19:43:24 +0300

    revert empty string

--- index.js
+++ index.js
@@ -645,7 +645,7 @@ bot.hears("💥", async (ctx) => {
       }
       return;
     }
-    await ctx.reply("\u200E", searchMenu);
+    await ctx.reply("💥 Супер-лайк надіслано!", searchMenu);
     user.seen = user.seen || [];
     if (!user.seen.includes(otherId)) user.seen.push(otherId);
     await saveUser(user);
@@ -672,7 +672,7 @@ bot.action("confirm_superlike", async (ctx) => {
   user.superLikesUsed.push(today);
   await saveUser(user);
   await handleLikeDislike(ctx, user, "like");
-  await ctx.reply("\u200E", searchMenu);
+  await ctx.reply("💥 Супер-лайк надіслано!", searchMenu);
 });
 
 // Обробник для скасування супер-лайка
@@ -1382,7 +1382,7 @@ async function handleSearch(ctx, user, id, isInline = false) {
       },
       ...photos.slice(1).map((file_id) => ({ type: "photo", media: file_id })),
     ]);
-    await ctx.reply("\u200E", searchMenu);
+    await ctx.reply("💝/❌ Зробіть свій вибір:", searchMenu);
   } catch (e) {
     console.error("handleSearch ERROR:", e);
     await ctx.reply("Виникла технічна помилка. Спробуйте ще раз.");
